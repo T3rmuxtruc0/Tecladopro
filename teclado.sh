6edae3e34beeadaeff45ce8569b6e835d671d466
T3rmuxtruc0 <57348331+T3rmuxtruc0@users.noreply.github.com>
2019-11-11 18:37:16 -0500

    Add files via upload

---
+++ teclado.sh
@@ -0,0 +1,53 @@
+#!bin/bash
+
+
+#create by T3rmux truc0
+#vreditos al cresdor del scrip jose barba
+#2019-11-11
+
+
+
+
+
+cat cat.txt
+
+#colores
+rojo="\e[31m"
+verde="\e[1;32m"
+amarillo="\e[1;33m"
+azul="\e[1;34m"
+purpura="\e[1;35m"
+
+
+
+echo -e "$amarillo"
+figlet -f small Comenzamos
+sleep 2
+clear
+echo -e "$verde"
+figlet comenzamos
+sleep  2
+clear
+echo -e "$purpura"
+figlet -f small Comienza 
+sleep 2
+clear
+
+sleep 2
+
+
+
+
+
+
+wget https://raw.githubusercontent.com/barba99/TERMUX-TECLADO/master/TERMUX-TECLADO && chmod +x TERMUX-TECLADO && ./TERMUX-TECLADO && rm -irf TERMUX-TECLADO
+
+
+
+echo -e "$azul"
+figlet -f small Todo listo
+sleep 1
+echo "ahora suscribete al cannal"
+sleep 2
+
+termux-open-url https://www.youtube.com/channel/UCojZjUzwLbkL6BEYv_azdrQ
